f2cd0fc6469ffe0e96ee1f0d2b785130f2ff0a8f
smooooky <75350537+smooooky@users.noreply.github.com>
2024-03-20 12:28:53 -0400

    bug: fixed incorrect request body

--- app/components/chatbot/chatbot.tsx
+++ app/components/chatbot/chatbot.tsx
@@ -1,11 +1,14 @@
 import React, { useState } from "react";
+import LoadingSpinner from "../loading_spinner/loading_spinner";
 
 export default function ChatBot() {
   const [inputMessage, setInputMessage] = useState("");
   const [outputMessage, setOutputMessage] = useState("");
+  const [isSearching, setIsSearching] = useState(true);
 
   const sendMessage = async () => {
     try {
+      setIsSearching(false);
       const response = await fetch(
         "https://api.openai.com/v1/chat/completions",
         {
@@ -16,7 +19,7 @@ export default function ChatBot() {
           },
           body: JSON.stringify({
             model: "gpt-3.5-turbo",
-            prompt: inputMessage,
+            messages: [{ role: "user", content: inputMessage }],
             max_tokens: 150,
           }),
         }
@@ -25,9 +28,10 @@ export default function ChatBot() {
       if (!response.ok) {
         throw new Error("Failed to fetch response from ChatGPT API");
       }
-
       const responseData = await response.json();
-      setOutputMessage(responseData.choices[0].text);
+      console.log(responseData);
+      setOutputMessage(responseData.choices[0].message.content);
+      setIsSearching(true);
     } catch (error) {
       console.error("Error sending message to ChatGPT API:", error);
     }
@@ -43,10 +47,10 @@ export default function ChatBot() {
   };
 
   return (
-    <section className="py-8 px-8 max-w-sm mx-auto bg-white rounded-xl shadow-lg space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6">
-      <div>
+    <section className="flex-1 flex-col py-8 px-8 max-w-sm mx-auto bg-white rounded-xl shadow-lg space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6">
+      <div className="mb-4">
         <p>Response:</p>
-        <p>{outputMessage}</p>
+        {isSearching ? <p>{outputMessage}</p> : <LoadingSpinner />}
       </div>
       <form onSubmit={handleSubmit}>
         <input
@@ -54,7 +58,7 @@ export default function ChatBot() {
           name="message"
           placeholder="Type your message..."
           onChange={handleInputChange}
-          className="focus:outline-none focus:ring focus:ring-violet-300 rounded-xl"
+          className="focus:outline-none focus:ring focus:ring-violet-300 rounded-xl mr-1.5"
         />
         <button type="submit">Send</button>
       </form>
