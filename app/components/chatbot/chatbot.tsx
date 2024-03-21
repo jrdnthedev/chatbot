d87644a945a8a7aa3d84a93178e9ca79e342821f
smooooky <75350537+smooooky@users.noreply.github.com>
2024-03-21 01:55:45 -0400

    refactor: added better spacing between elements and some font updates

--- app/components/chatbot/chatbot.tsx
+++ app/components/chatbot/chatbot.tsx
@@ -49,18 +49,36 @@ export default function ChatBot() {
   return (
     <section className="flex-1 flex-col py-8 px-8 max-w-sm mx-auto bg-white rounded-xl shadow-lg space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6">
       <div className="mb-4">
-        <p>Response:</p>
-        {isSearching ? <p>{outputMessage}</p> : <LoadingSpinner />}
+        {isSearching ? (
+          <>
+            <div className="mb-2">
+              <p>Question:</p>
+              <p>{inputMessage}</p>
+            </div>
+            <div>
+              <p>Response:</p>
+              <p>{outputMessage}</p>
+            </div>
+          </>
+        ) : (
+          <LoadingSpinner />
+        )}
       </div>
-      <form onSubmit={handleSubmit}>
+
+      <form onSubmit={handleSubmit} className="flex mx-0">
         <input
           type="text"
           name="message"
           placeholder="Type your message..."
           onChange={handleInputChange}
-          className="focus:outline-none focus:ring focus:ring-violet-300 rounded-xl mr-1.5"
+          className="focus:outline-none focus:ring focus:ring-violet-300 rounded-xl mr-1.5 flex-1"
         />
-        <button type="submit">Send</button>
+        <button
+          type="submit"
+          className="p-1.5 bg-green-300 font-medium text-white font-sans"
+        >
+          Send
+        </button>
       </form>
     </section>
   );
